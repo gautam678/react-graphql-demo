56b819c875091a5d79049c754ee683b79991c6b9
g0s00t9 <gautam.somappa@walmart.com>
2019-08-02 11:09:49 -0400

    Initial commit

---
+++ frontend/components/Nav.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React, { Component } from "react";
+import NavStyles from "./styles/NavStyles";
+class Nav extends Component {
+  render() {
+    return (
+      <NavStyles>
+        <Link href="/items">
+          <a>Items</a>
+        </Link>
+        <Link href="/sell">
+          <a>Sell</a>
+        </Link>
+        <Link href="/signup">
+          <a>Signup</a>
+        </Link>
+        <Link href="/orders">
+          <a>Orders</a>
+        </Link>
+        <Link href="/me">
+          <a>Account</a>
+        </Link>
+      </NavStyles>
+    );
+  }
+}
+
+export default Nav;
---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const Home = props => (
+  <div>
+    <p>Home</p>
+  </div>
+);
+
+export default Home;
