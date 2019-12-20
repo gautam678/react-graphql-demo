cb7df29c9fc9d159d3edd67c1000d541d2bce2df
g0s00t9 <gautam.somappa@walmart.com>
2019-12-19 22:26:42 -0500

    Setup Yoga and create dummy schema
    Getting the graphQL server running

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,23 @@
 // let's go!
+const dotenv = require("dotenv");
+
+require("dotenv").config({ path: "variables.env" });
+
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+//TODO use express middleware to handle cookies
+
+// TODO use express middleware to populate current users
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  details => console.log(`server is running on port ${details.port}`)
+);
