d0f7c788d5957e8d085ed218b392f4d49ddc6c44
gautam678 <gautam678@gmail.com>
2019-12-25 23:05:34 -0500

    Server side graphQL
    Completed code till server side graphQL

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if user is logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   const newDogs = { name: args.name };
+  //   global.dogs.push(newDogs);
+  //   return newDogs;
+  // }
+};
 
 module.exports = Mutation;
